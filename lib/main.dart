a8841fd0495ee3d1b4d1debcb09fa3448ac37d49
kobanya <nagy.bela.budapest@gmail.com>
2022-11-05 17:15:32 +0100

    két szövegesmező hozzáadása és elrendezése

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MaterialApp(
   home: nevjegy()
 ));
 
@@ -10,9 +10,39 @@ class nevjegy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightGreen,
       appBar: AppBar(
-        title: Text ('Névjegyem'),
+        title: const Text ('Névjegyem'),
+        centerTitle: true,
+        backgroundColor: Colors.green[900],
       ),
+    body: Padding(
+      padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Text('NÉV',
+            style: TextStyle(
+              color: Colors.black45,
+              letterSpacing: 2.0,
+            ),
+          ),
+          // Üres tér beillesztése a két névelem közé
+        SizedBox(height: 10.0),
+        Text('Díno Nínó a dinoszaurusz',
+            style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 3.0,
+            fontWeight:  FontWeight.bold,
+            fontSize: 28.0,
+          ),
+        ),
+            SizedBox(height: 10.0),
+
+
+        ],
+      ),
+    )
     );
 
   }
